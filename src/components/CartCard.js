bcc2076e7748c16728530328839edd328ff4e273
Erik Mejia <erikdojo352@gmail.com>
2023-09-07 19:26:36 -0400

    Add / Remove Reducers added

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,14 +1,17 @@
+import { remove } from "../store/cartSlice";
+import { useDispatch } from "react-redux";
 import "./CartCard.css";
 
-export const CartCard = ({product}) => {
-  const {name, price, image} = product;
+export const CartCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const { name, price, image } = product;
 
   return (
-      <div className="cartCard">
-        <img src={image} alt={name} />
-        <p className="productName">{name}</p>
-        <p className="productPrice">${price}</p>
-        <button>Remove</button>
-      </div>
-  )
-}
+    <div className="cartCard">
+      <img src={image} alt={name} />
+      <p className="productName">{name}</p>
+      <p className="productPrice">${price}</p>
+      <button onClick={() => dispatch(remove(product))}>Remove</button>
+    </div>
+  );
+};
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -4,8 +4,18 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: { cartList: [], total: 0 },
   reducers: {
-    add(state, action) {},
-    remove(state, action) {},
+    add(state, action) {
+      const updatedCartList = state.cartList.concat(action.payload);
+      const updatedTotal = state.total + action.payload.price;
+      return { ...state, cartList: updatedCartList, total: updatedTotal };
+    },
+    remove(state, action) {
+      const updatedCartList = state.cartList.filter(
+        (item) => item.id !== action.payload.id
+      );
+      const updatedTotal = state.total - action.payload.price;
+      return { ...state, cartList: updatedCartList, total: updatedTotal };
+    },
   },
 });
 
